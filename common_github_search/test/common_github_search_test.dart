ca9c5cb5517d87a0964e88d433dc43e55e27f554
Billy Huang <10646003@ntub.edu.tw>
2024-02-29 18:25:59 +0800

    chore(common): update with dart package creater

--- common_github_search/test/common_github_search_test.dart
+++ common_github_search/test/common_github_search_test.dart
@@ -1,12 +1,17 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 import 'package:common_github_search/common_github_search.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
   });
 }
